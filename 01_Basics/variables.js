5ed13e83b034374556348f676dcf8f5d9006d92d
Arpit Vaish <156198699+apricotarpit@users.noreply.github.com>
2024-09-24 17:30:18 +0000

    variables

---
+++ 01_Basics/variables.js
@@ -0,0 +1,18 @@
+const accountID = 144553
+let accountemail="arpit123@gmial.com"
+var accountpassward="12345"
+accountcity="jaipur"
+
+// accountID=2
+accountemail="hcdc@gmial.com"
+accountpassward="fjhdfkl"
+accountcity="djkd;"
+
+console.log(accountID);
+console.log(accountemail);
+console.log(accountpassward);
+console.log(accountcity);
+
+
+console.table([accountID,accountemail,accountpassward,accountcity]);
+
