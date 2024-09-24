f8dff52b5adcf920843e90b183bd1265a69889da
Arpit Vaish <156198699+apricotarpit@users.noreply.github.com>
2024-09-24 17:29:21 +0000

    datatypes

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS Code as newer vrsion of the JS 
+
+// alert(3+3);  // wew are using NodeJS not Browser
+
+console.log(3+3)
+console.log("hitesh");
+
+let name="Hitesh"
+let age=18
+let isloggedin=false
+
+//  all Types of Data Types
+
+// number=> 2 to power 53
+// big int 
+//  string => ""
+//  boolean => true / false
+// NULL => stand alone value (represent the empty value)
+//  undefined=> 
+//  symbol => to define uniqueness
+//  Object
+
+console.log(typeof "hitesh");
+console.log(typeof age);
+console.log(typeof isloggedin);
+
+console.log(typeof NULL);
+console.log( typeof undefined);
+
+
+
+
+
