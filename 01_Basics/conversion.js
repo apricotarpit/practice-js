fbdacd43046c9fdabeb7d356520644f70db1d38b
Arpit Vaish <156198699+apricotarpit@users.noreply.github.com>
2024-09-25 09:21:18 +0000

    conversion

---
+++ 01_Basics/conversion.js
@@ -0,0 +1,35 @@
+let score= "undefined";
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valuenumber=Number(score);
+console.log(typeof valuenumber);
+
+console.log(valuenumber);
+
+/*
+"33" => 33
+"33absc"=> NaN
+true => 1; false=>0
+
+*/
+
+let isloggedin=""
+
+let booleanisloggedin= Boolean(isloggedin)
+
+console.log(booleanisloggedin);
+
+// 1=> true 0=> false
+// "" =>false
+// " sdjs" => true
+
+let somenumber=33
+
+let stringnum=String(somenumber);
+console.log(stringnum);
+
+
+
+
