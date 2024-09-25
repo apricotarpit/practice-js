a5e9fb982eb546d1cec0687c2e65a1f7edf6c413
Arpit Vaish <156198699+apricotarpit@users.noreply.github.com>
2024-09-25 09:35:59 +0000

    opreation

---
+++ 01_Basics/operation.js
@@ -0,0 +1,24 @@
+
+let value=3
+let negValue= -value
+
+console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(3%2)
+
+let str1="Hello"
+let str2=" world"
+
+let str3=str1+str2;
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log(1+2+2+"2");
+console.log("1"+3+2);
+
