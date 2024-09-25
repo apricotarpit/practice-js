ef5ee9e6a6d87273579c6cc45333b1e06b650f69
Arpit Vaish <156198699+apricotarpit@users.noreply.github.com>
2024-09-25 10:57:06 +0000

    datatypessummary

---
+++ 01_Basics/DatatypesSummary.js
@@ -0,0 +1,32 @@
+//  Primitive
+
+//  7 types :string,Number ,Boolean,NULL,undefined,Symbol, Bigint
+
+
+// JavaScript is a Dynamically type lang.
+
+
+// Reference (Non Primitive)
+
+// Array ,Objects,Functions
+
+const id =Symbol('123');
+const anotherid=Symbol('123');
+
+console.log(id===anotherid);
+
+// for declaring the number to Bigint
+const Bignumber= 3456534536546n;
+console.log(Bignumber);
+
+// Array, Objects, Functions
+
+const heros=["Shaktiman","naagraj","Doga"];
+let Myobj={
+    name: 'Hitesh',
+    age:22,
+    id:'123'
+}
+console.log(Myobj);
+console.log(heros);
+
