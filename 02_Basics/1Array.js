88957f0116434cf96f96bb1fb4c7c75941cb62b7
Arpit Vaish <156198699+apricotarpit@users.noreply.github.com>
2024-09-28 05:18:16 +0000

    array basics

---
+++ 02_Basics/1Array.js
@@ -0,0 +1,41 @@
+// Array
+
+const arr=[0,1,2,3,4,5]
+
+const myhero=["shaktiman","nagraj"]
+
+const myarr=new Array(1,2,3,4)
+
+
+// Array method
+// arr.push(6)
+// arr.push(7)
+// arr.pop()
+
+// arr.unshift(9)
+// arr.shift()
+
+
+
+// console.log(arr.includes(8));
+// console.log(arr.indexOf(3));
+
+// const newarr=arr.join()
+// console.log(arr)
+// console.log(newarr)
+// console.log(typeof newarr);
+
+//  Slice and splice
+
+console.log("A",arr);
+const myn1=arr.slice(1,3);
+
+console.log(myn1);
+console.log("B",arr);
+
+const myn2=arr.splice(1,3);
+console.log("C",arr);
+console.log(myn2);
+
+
+
