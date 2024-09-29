1f8e08af5cb04e697fee984efda260a2ac0ee869
Arpit Vaish <156198699+apricotarpit@users.noreply.github.com>
2024-09-29 17:19:34 +0000

    api basics

---
+++ 02_Basics/APIBasics.js
@@ -0,0 +1,28 @@
+// destructuring of the array
+const course={
+    name:"js in hindi",
+    prince:888,
+    instructor:"hitesh"
+}
+console.log(course.instructor);
+console.log(course.prince);
+
+const {instructor:ins}=course;
+console.log(ins);
+
+// apna kaam dusro pr chorr dena =>API
+// API structure => key nd value both are in string
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
+
+// api are come in form of XMl and JSON 
+// JSON formatter =>java script object notation
